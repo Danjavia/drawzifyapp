ddd294fa166e0e55315b91d72646fdf9b884d2de
Danny Viasus <danjavia.iconshock@gmail.com>
2014-03-18 21:12:47 -0500

    initial commit

---
+++ js/app.js
@@ -0,0 +1,21 @@
+var myapp = angular.module( 'myapp', [ 'firebase' ]);
+
+myapp.controller( 'BookCtrl', [ '$scope', 'angularFire',  
+  	function BookCtrl( $scope, angularFire ) {
+			var url = 'https://<yourID>.firebaseio.com/';
+			var promise = angularFire( url, $scope, 'books', [] );
+			$scope.newBook = {};
+		
+			promise.then( function() {
+				startWatch( $scope );
+			});
+  	}
+]);
+
+function startWatch($scope) {  
+ 	$scope.add = function() {
+		console.log($scope.newBook);
+		$scope.books.push($scope.newBook);
+		$scope.newBook = '';
+ 	}
+}
