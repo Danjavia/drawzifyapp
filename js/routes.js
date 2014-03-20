b6c31f4a24e4b787b5fc0341ee8c88966a5b0f6c
= <danjavia.iconshock@gmail.com>
2014-03-20 13:37:24 -0500

    make draw view

--- js/routes.js
+++ js/routes.js
@@ -5,26 +5,32 @@ angular.module('myApp.routes', ['ngRoute'])
    // configure views; the authRequired parameter is used for specifying pages
    // which should only be available while logged in
    .config(['$routeProvider', function($routeProvider) {
-      $routeProvider.when('/home', {
+      $routeProvider.when( '/home', {
          templateUrl: 'partials/home.html',
          controller: 'HomeCtrl'
       });
 
-      $routeProvider.when('/chat', {
+      $routeProvider.when( '/chat', {
          templateUrl: 'partials/chat.html',
          controller: 'ChatCtrl'
       });
 
-      $routeProvider.when('/account', {
+      $routeProvider.when( '/account', {
          authRequired: true, // must authenticate before viewing this page
          templateUrl: 'partials/account.html',
          controller: 'AccountCtrl'
       });
 
-      $routeProvider.when('/login', {
+      $routeProvider.when( '/login', {
          templateUrl: 'partials/login.html',
          controller: 'LoginCtrl'
       });
 
-      $routeProvider.otherwise({redirectTo: '/home'});
+      $routeProvider.when( '/draw', {
+         //authRequired: true, // must authenticate before viewing this page
+         templateUrl: 'partials/draw.html',
+         // controller: 'LoginCtrl'
+      });
+
+      $routeProvider.otherwise({ redirectTo: '/home'});
    }]);
